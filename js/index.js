c902a69fc58438f56a38689031990b13d7526d5d
rnilsen77 <80304198+rnilsen77@users.noreply.github.com>
2021-08-14 18:15:44 -0600

    file transfer

---
+++ js/index.js
@@ -0,0 +1,9 @@
+console.log("it worked kekw :D");
+
+$("#button1").on("click", function(){
+    $("#tourGuide_words").fadeIn();
+});
+
+$("#button2").on("click", function(){
+    $("#procon_words").fadeIn();
+});
