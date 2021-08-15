4e9c4a46656d873e6a33769a6c102909d8340e21
rnilsen77 <80304198+rnilsen77@users.noreply.github.com>
2021-08-14 21:10:46 -0600

    Final Product

--- js/index.js
+++ js/index.js
@@ -2,8 +2,10 @@ console.log("it worked kekw :D");
 
 $("#button1").on("click", function(){
     $("#tourGuide_words").fadeIn();
+    $("#button1").fadeOut();
 });
 
 $("#button2").on("click", function(){
     $("#procon_words").fadeIn();
+    $("#button2").fadeOut();
 });
